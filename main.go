8556c74e394de45864fd2b1c0ea31257f17c684e
Georgi Knox <georgicodes@github.com>
2017-10-10 12:14:41 -0400

    move examples into packages

--- main.go
+++ main.go
@@ -2,64 +2,12 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"sync"
+
+	"github.com/georgicodes/hello-go-concurrency/pinger"
 )
 
-func init() {
-	// When set to 1, a single logical processor for scheduler to use,
-	// meaning only 1 goroutine is running at any given time.
-	//
-	// When set to 2, the goroutines will run in parallel as there will be
-	// 2 processors with a thread each.
-	runtime.GOMAXPROCS(2)
-}
-
 func main() {
-	// a WaitGroup is a synchronous counting semaphore, used to manage concurrency
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	fmt.Println("Start goroutines")
-
-	// create an annoymous goroutine
-	// go here will schedule this goroutine to run in the LRQ on P
-	go func() {
-		countdown()
-		wg.Done()
-	}()
-
-	// create another annoymous goroutine
-	go func() {
-		countup()
-		wg.Done()
-	}()
-
-	// at this point we have 3 goroutines running concurrently (main, lowercase, uppercase)
-
-	fmt.Println("waiting to finish")
-	// when main exits, the whole program exits, wg.Wait(), will wait for the goroutines
-	// to finish. It prevents main from terminating until other two goroutines are finished.
-	// When that happens it will wake up the main goroutine and terminate.
-	wg.Wait()
-
+	fmt.Println("Starting program")
+	pinger.Run()
 	fmt.Println("\nTerminating program")
-}
-
-func countdown() {
-	fmt.Println("countdown")
-	i := 100
-	for i >= 0 {
-		fmt.Printf("%d:%s ", i, "UP")
-		i = i - 1
-	}
-}
-
-func countup() {
-	fmt.Println("countup")
-	i := 0
-	for i <= 100 {
-		fmt.Printf("%d:%s ", i, "dn")
-		i = i + 1
-	}
 }
