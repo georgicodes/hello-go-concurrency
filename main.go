37da6b59d6f1ee876634b1abf37ac6adb7eb57e6
Georgi Knox <georgicodes@github.com>
2017-10-11 08:43:25 -0400

    channel synchronization example

--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/georgicodes/hello-go-concurrency/pinger"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	fmt.Println("Starting program")
-	pinger.Run()
-	fmt.Println("\nTerminating program")
+
+	// sig channel receives Unix signals
+	sig := make(chan os.Signal, 1)
+	// done channel is used to notify when the program can exit
+	done := make(chan bool, 1)
+
+	// signal.Notify registers the given channel to receive notifications of the specified signals.
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		// program will block waiting for a signal
+		signal := <-sig
+		fmt.Println()
+		fmt.Println(signal)
+		// send a value to notify that we are done
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	// block from exiting until we receive a notification.
+	// If this wasn't here, the program would exit before the above goroutine would even run
+	<-done
+	fmt.Println("exiting")
 }
